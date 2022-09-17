a98b74d44598d3b31f627c4eb41bbf15af78d392
lesyamosk <113190653+lesyamosk@users.noreply.github.com>
2022-09-17 12:24:02 +0300

    6

--- lesson2_homework/FIB_recursion.py
+++ lesson2_homework/FIB_recursion.py
@@ -1,1 +1,9 @@
-print('dc')
+def FIB_recursion(n):
+    if n < 1:
+        return
+    if n == 1:
+        return 0
+    if n == 2:
+        return 1
+    else:
+        return FIB_recursion(n-1) + FIB_recursion(n-2)
